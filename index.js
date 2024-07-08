b182124c13291bab384efd27f9d7b8637308d41e
Mishary Morished <mimorished@gmail.com>
2024-07-08 00:34:01 +0000

    Project

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: './public/data/uploads/' })
 
 var app = express();
 
@@ -12,6 +14,13 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
